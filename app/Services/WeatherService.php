5a2c34e7080039b8a0911e3b5093a807da822ff4
Lukasz Skalik <luui321@gmail.com>
2022-11-21 21:57:03 +0100

    Update WeatherService.php

--- app/Services/WeatherService.php
+++ app/Services/WeatherService.php
@@ -9,10 +9,8 @@ class WeatherService
 {
     public function getKrakow()
     {
-        $response = Http::get('https://api.openweathermap.org/data/2.5/weather?q=krakow&appid=8a38d91329556eba81fe4f70f628247b&units=metric');
+        $response = Http::get('https://api.openweathermap.org/data/2.5/weather?q=krakow&appid=&units=metric');
 
-
-        //$response = $client->request('GET', 'https://api.openweathermap.org/data/2.5/weather?q=krakow&appid=8a38d91329556eba81fe4f70f628247b&units=metric');
         if ($response->status() == 200)
         {
             $content = json_decode($response->getBody());
